3d004c1afa6919e4d53d04d67e33a7b79581eb39
hanadumal <47523442+hanadumal@users.noreply.github.com>
2023-01-19 16:53:06 +0800

    Update install_kubeadm.sh

--- install_kubeadm.sh
+++ install_kubeadm.sh
@@ -10,7 +10,9 @@ EOF
 sudo apt update
 
 # 获取kubeadm、kubelet、kubectl, 使用1.23.3版本
+echo "安装kubeadm kubelet kubectl"
 sudo apt install -y kubeadm=1.23.3-00 kubelet=1.23.3-00 kubectl=1.23.3-00
 
 # 使用apt-mark hold, 锁定这三个软件的版本
+echo "锁定版本"
 sudo apt-mark hold kubeadm kubelet kubectl
